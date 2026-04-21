2850293548f1be43ce27be0b59c03ecb66d82aea
Sai Ram Vadranapu <sairamvadranapu@Sais-MacBook-Pro.local>
2026-04-21 19:04:25 +0530

    Fixed indentation error in frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,10 @@
 import streamlit as st
-import sys
-import os
-
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, ROOT_DIR)
-
 import requests
 
 st.set_page_config(page_title="MF FAQ Assistant", layout="centered")
+
+# Backend API URL (REPLACE with your Render URL)
+API_URL = "https://rag-mf-chatbot-1.onrender.com/"
 
 # Title
 st.title("Mutual Fund FAQ Assistant")
@@ -26,9 +23,8 @@ query = st.text_input("Ask your question:")
 
 if query:
     with st.spinner("Fetching answer..."):
-        API_URL = "https://rag-mf-chatbot-1.onrender.com/"
+        response = requests.post(API_URL, json={"question": query})
+        answer = response.json()["answer"]
 
-response = requests.post(API_URL, json={"question": query})
-answer = response.json()["answer"]
         st.markdown("### Answer")
         st.write(answer)
